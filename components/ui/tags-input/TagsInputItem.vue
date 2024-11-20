02bcd86af32c638a5285d349f2435c9fc20304a5
Milos Savic <msavic248@yahoo.com>
2024-11-20 23:34:32 +0100

    Initial commit with shadcn-vue

---
+++ components/ui/tags-input/TagsInputItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { TagsInputItem, type TagsInputItemProps, useForwardProps } from 'radix-vue'
+
+import { computed, type HTMLAttributes } from 'vue'
+
+const props = defineProps<TagsInputItemProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <TagsInputItem v-bind="forwardedProps" :class="cn('flex h-6 items-center rounded bg-stone-100 data-[state=active]:ring-stone-950 data-[state=active]:ring-2 data-[state=active]:ring-offset-2 ring-offset-white dark:bg-stone-800 dark:data-[state=active]:ring-stone-300 dark:ring-offset-stone-950', props.class)">
+    <slot />
+  </TagsInputItem>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <h1 class="stone-100">
+        Welcome to Nuxt.js!
+    </h1>
+	<DropdownMenu>
+		<DropdownMenuTrigger as-child>
+			<Button variant="outline">
+				Open
+			</Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent class="w-56">
+		<DropdownMenuLabel>My Account</DropdownMenuLabel>
+		<DropdownMenuSeparator />
+		<DropdownMenuItem>Profile</DropdownMenuItem>
+		<DropdownMenuItem>Billing</DropdownMenuItem>
+		<DropdownMenuItem>Team</DropdownMenuItem>
+		<DropdownMenuItem>Subscription</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
+    
+</template>
