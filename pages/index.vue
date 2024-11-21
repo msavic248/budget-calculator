ce7f6c2254d7e12d972f98123c584957760f0d77
Milos <msavic248@yahoo.com>
2024-11-21 21:14:14 +0100

    Updated layout with template dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,112 @@
 <script setup lang="ts">
-
-
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Activity, ArrowUpRight, CircleUser, CreditCard, DollarSign, Menu, Package2, Search, Users } from 'lucide-vue-next'
 </script>
 
 <template>
-    <h1 class="stone-100">
-        Welcome to Nuxt.js!
-    </h1>
-	<DropdownMenu>
-		<DropdownMenuTrigger as-child>
-			<Button variant="outline">
-				Open
-			</Button>
-		</DropdownMenuTrigger>
-		<DropdownMenuContent class="w-56">
-		<DropdownMenuLabel>My Account</DropdownMenuLabel>
-		<DropdownMenuSeparator />
-		<DropdownMenuItem>Profile</DropdownMenuItem>
-		<DropdownMenuItem>Billing</DropdownMenuItem>
-		<DropdownMenuItem>Team</DropdownMenuItem>
-		<DropdownMenuItem>Subscription</DropdownMenuItem>
-		</DropdownMenuContent>
-	</DropdownMenu>
-    
+  <div class="flex min-h-screen w-full flex-col bg-gray-100">
+    <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-white px-4 md:px-6">
+      <nav class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <a
+          href="#"
+          class="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Package2 class="h-6 w-6" />
+          <span class="sr-only">Acme Inc</span>
+        </a>
+        <a
+          href="#"
+          class="text-foreground transition-colors hover:text-foreground"
+        >
+          Dashboard
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Income
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Expenditure
+        </a>
+      </nav>
+      <Sheet>
+        <SheetTrigger as-child>
+          <Button
+            variant="outline"
+            size="icon"
+            class="shrink-0 md:hidden"
+          >
+            <Menu class="h-5 w-5" />
+            <span class="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav class="grid gap-6 text-lg font-medium">
+            <a
+              href="#"
+              class="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Package2 class="h-6 w-6" />
+              <span class="sr-only">Acme Inc</span>
+            </a>
+            <a href="#" class="hover:text-foreground">
+              Dashboard
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Income
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Expenditure
+            </a>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="secondary" size="icon" class="rounded-full ml-auto">
+              <CircleUser class="h-5 w-5" />
+              <span class="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+    <main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+	<Card>
+		<CardHeader>
+			<CardTitle>Dashboard</CardTitle>
+			<CardDescription>Overview of your app</CardDescription>
+		</CardHeader>
+		<CardContent>
+			
+		</CardContent>
+	</Card>
+    </main>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ const animate = require("tailwindcss-animate")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx,vue}',
     './components/**/*.{ts,tsx,vue}',
     './app/**/*.{ts,tsx,vue}',
     './src/**/*.{ts,tsx,vue}',
 	],
+  darkMode: ["class"],
   prefix: "",
   theme: {
     container: {
