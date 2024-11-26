c957ead2cb2bba4cd8ac2a21eaef0b8397788827
Milos Savic <msavic248@yahoo.com>
2024-11-26 22:13:00 +0100

    Reverted button styling

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -8,15 +8,15 @@ export const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-emerald-900 text-emerald-50 shadow hover:bg-emerald-900/90 dark:bg-emerald-50 dark:text-emerald-900 dark:hover:bg-emerald-50/90',
+          'bg-stone-900 text-stone-50 shadow hover:bg-stone-900/90 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/90',
         destructive:
-          'bg-red-500 text-emerald-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-emerald-50 dark:hover:bg-red-900/90',
+          'bg-red-500 text-stone-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-stone-50 dark:hover:bg-red-900/90',
         outline:
-          'border border-stone-200 bg-white shadow-sm hover:bg-emerald-100 hover:text-emerald-900 dark:border-stone-800 dark:bg-emerald-950 dark:hover:bg-emerald-800 dark:hover:text-emerald-50',
+          'border border-stone-200 bg-white shadow-sm hover:bg-stone-100 hover:text-stone-900 dark:border-stone-800 dark:bg-stone-950 dark:hover:bg-stone-800 dark:hover:text-stone-50',
         secondary:
-          'bg-emerald-100 text-emerald-900 shadow-sm hover:bg-emerald-100/80 dark:bg-emerald-800 dark:text-emerald-50 dark:hover:bg-emerald-800/80',
-        ghost: 'hover:bg-emerald-100 hover:text-emerald-900 dark:hover:bg-emerald-800 dark:hover:text-emerald-50',
-        link: 'text-emerald-900 underline-offset-4 hover:underline dark:text-emerald-50',
+          'bg-stone-100 text-stone-900 shadow-sm hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80',
+        ghost: 'hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-800 dark:hover:text-stone-50',
+        link: 'text-stone-900 underline-offset-4 hover:underline dark:text-stone-50',
       },
       size: {
         default: 'h-9 px-4 py-2',
